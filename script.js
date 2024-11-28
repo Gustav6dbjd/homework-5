94f2180cd648cdf988bf06f872173454c02af4f8
Максим <141343669+Gustav6dbjd@users.noreply.github.com>
2024-11-28 18:21:56 +0300

    tasks from lesson

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+// номер 6
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.includes(3));
+
+
+// номер 7
+// const arr = [1, 2, 3, 4, 5];
+// const result = arr.find(x => x > 3);
+// console.log(result);
+
+
+// номер 8
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.findIndex(x => x > 3)); 
+// console.log(arr.findLastIndex(x => x > 3));
+
+
+
+// номер 9
+// const arr = [1, 2, 3, 4, 5];
+// const newArr = arr.filter(x => x > 3);
+// console.log(newArr);
+
+
+// конец презентации:
+
+// номер 1
+// const numbers = [1, 2, 3, 4, 5];
+// const doubleNumbers = numbers.map(num => num * 2);
+// console.log(doubleNumbers);
+
+
+// номер 2
+// let numbers = [3, 1, 4, 1, 5, 9, 2, 6];
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+
+//номер 3
+// let words = ['hello', 'world'];
+// words.reverse();
+// console.log(strings);
+
+
+//номер 4
+// let fruits = 'apple,banana,cherry';
+// let fruit = fruits.split(',');
+// console.log(fruit);
+
+
+// номер 5
+// let words = ['hello', 'world'];
+// let sentence = words.join(' ');
+// console.log(sentence);
+
